3623c82d18367b1a027c31b77d681b975251b141
chollingsworth <caleb.hollingsworth10@gmail.com>
2020-11-10 15:26:41 -0600

    completed take a ten minute walk

--- solutions.js
+++ solutions.js
@@ -59,3 +59,30 @@
 // 	}
 // 	return array.join(' ');
 // }
+
+//11/10/20 kyu = 6
+
+//Prompt: "Take a Ten Minute Walk" -- You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+//solution:
+//function isValidWalk(walk) {
+//   let snCounter = 0;
+//   let ewCounter = 0;
+// if(walk.length !== 10) {
+//   return false
+// } else if(walk.length == 10) {
+//   for(let i = 0; i < walk.length; i++) {
+//     if(walk[i] === 'n') {
+//       snCounter = snCounter + 1;
+//     }if(walk[i] === 's') {
+//       snCounter = snCounter - 1;
+//     }if(walk[i] === 'e') {
+//       ewCounter = ewCounter + 1;
+//     }if(walk[i] === 'w') {
+//       ewCounter = ewCounter - 1;
+//     }
+//   }
+//   if(ewCounter === 0 && snCounter === 0) {
+//     return true
+//   }else {return false}
+// }
